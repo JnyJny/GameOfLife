23ef700e6a1f3d61c3f71ff1028494c70d34f695
Erik O'Shaughnessy <erik.oshaughnessy@gmail.com>
2015-10-24 22:16:27 -0500

    Worldopt renamed to OptimizedWorld

--- GameOfLife/__init__.py
+++ GameOfLife/__init__.py
@@ -1,11 +1,15 @@
+'''Conway's Game of Life
 
-# Conway's Game of Life
+See: https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life
 
+This particular Game of Life is implemented as a two dimensional
+world populated with cells.
+'''
 
 import time
 
 from .cell import Cell as Cell
-from .world import WorldOpt as World
+from .world import OptimizedWorld as World
 from .patterns import Patterns as Patterns
 
 __all__ = [ 'Cell','World','Patterns']
