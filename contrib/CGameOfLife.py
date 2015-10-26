07c482cc422d9d00b6000fa6a3a781d25392d80c
Erik O'Shaughnessy <erik.oshaughnessy@gmail.com>
2015-10-26 15:50:43 -0500

    fixed addPattern call

--- contrib/CGameOfLife.py
+++ contrib/CGameOfLife.py
@@ -28,6 +28,7 @@ class CursesWorld(World):
 
     def colorForCell(self,cell):
         '''
+        Returns a curses color_pair for a cell, chosen by the cell's age.
         '''
 
         n = min(cell.age // 100,len(self.colors)-1)
@@ -35,11 +36,24 @@ class CursesWorld(World):
         return curses.color_pair(n+1)
 
     def handle_input(self):
+        '''
+        Accepts input from the user and acts on it.
+        '''
         c = self.w.getch()
-        if c == ord('q'):
+        if c == ord('q') or c == ord('Q'):
             exit()
 
     def step(self):
+        '''
+        :return: set of cells current alive
+
+        This method advances the simulation one generation.
+
+        Calculates the number of generations per second (gps)
+        by timing how long the current step took to complete
+        and taking the inverse. 
+
+        '''
         t0 = time.time()
         ret = super(CursesWorld,self).step()
         t1 = time.time()
@@ -48,6 +62,9 @@ class CursesWorld(World):
             
     @property
     def status(self):
+        '''
+        Format string for the status line.
+        '''
         try:
             return self._status
         except AttributeError:
@@ -63,7 +80,12 @@ class CursesWorld(World):
 
     def draw(self):
         '''
+        :return: None
 
+        Updates each character at r,c in the curses window with
+        the appropriate colored marker for each cell in the world.
+
+        Moves the cursor to bottom-most line, left-most column.
         '''
         for y in range(self.height):
             for x in range(self.width):
@@ -77,10 +99,23 @@ class CursesWorld(World):
 
 
         self.w.move(self.height,1)
-        self.w.refresh()
+        
 
     def run(self,stop=-1,interval=0):
         '''
+        :param: stop     - optional integer
+        :param: interval - optional integer
+        :return: None
+
+        This method will run the simulation described by world until the
+        given number of generations specified by ''stop'' has been met. 
+        The default value will cause the simulation to run until interrupted
+        by the user. 
+
+        The interval is number of milliseconds to pause between generations.
+        The default value of zero allows the simulation to run as fast as
+        possible.
+
         '''
         self.w.clear()
         try:
@@ -90,7 +125,11 @@ class CursesWorld(World):
                 self.handle_input()
                 self.step()
                 self.draw()
-                curses.napms(interval)
+                
+                self.w.refresh()
+                
+                if interval:
+                    curses.napms(interval)
         except KeyboardInterrupt:
             pass
         
@@ -110,7 +149,7 @@ def main(stdscr,argv):
                 x,y = map(int,where.split(','))
         except:
             pass
-        w.addPattern(Patterns[name],x,y)
+        w.addPattern(Patterns[name],x=x,y=y)
 
     stdscr.nodelay(True)
         
