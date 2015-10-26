e7c6197162be13a82e57c3cf363908c28cc11044
Erik O'Shaughnessy <erik.oshaughnessy@gmail.com>
2015-10-26 15:34:24 -0500

    initial commit

---
+++ tests/test_cell.py
@@ -0,0 +1,151 @@
+
+
+import unittest
+
+from GameOfLife import Cell
+
+class CellTestCase(unittest.TestCase):
+
+    def assertIsCell(self,obj,x=None,y=None,alive=None,markers=None):
+        self.assertIsInstance(obj,Cell)
+        if x is not None:
+            self.assertEqual(obj.location[0],x)
+        if y is not None:
+            self.assertEqual(obj.location[1],y)
+        if alive is not None:
+            self.assertEqual(obj.alive,alive)
+        if markers is not None:
+            self.assertEqual(obj.markers,markers)
+
+
+class CellCreationTestCase(CellTestCase):
+
+    def testCellCreation(self):
+
+        with self.assertRaises(TypeError):
+            cell = Cell()
+
+        with self.assertRaises(TypeError):            
+            cell = Cell(0)
+
+        self.assertIsCell(Cell(0,0),x=0,y=0,alive=False,markers=' .')
+
+        alive = True
+        self.assertIsCell(Cell(0,0,alive=alive),alive=alive)
+
+        markers = 'ox'
+        cell = Cell(0,0,markers=markers)
+        self.assertIsCell(cell,markers=markers)
+
+        self.assertTrue(str(cell) == markers[0])
+        cell.alive = True
+        self.assertTrue(str(cell) == markers[1])
+
+class CellPropertyTestCase(CellTestCase):
+
+    def testCellAliveProperty(self):
+        cell = Cell(0,0)
+
+        cell.alive = True
+        cell.age = 1
+        self.assertTrue(cell.alive)
+
+        cell.alive = False
+        self.assertFalse(cell.alive)
+        self.assertEqual(cell.age,0)
+
+    def testCellNeighborsProperty(self):
+        x,y = 3,3
+        cell = Cell(x,y)
+
+        self.assertEqual(len(cell.neighbors),8)
+
+class CellMethodsTestCase(CellTestCase):
+
+    def testCellUpdateMethod(self):
+
+        cell = Cell(0,0)
+
+        for n in range(9):
+            cell.update(n)
+            self.assertEqual(cell.aliveNeighbors,n)
+            self.assertFalse(cell.alive)
+            self.assertEqual(cell.age,0)
+
+        cell.alive = True
+
+        for n in range(9):
+            cell.update(n)
+            self.assertEqual(cell.aliveNeighbors,n)
+            self.assertTrue(cell.alive)
+            self.assertEqual(cell.age,n+1)
+
+    def testCellCommitMethod(self):
+
+        cell = Cell(0,0)
+
+        for n in range(9):
+            cell.alive = False
+            cell.update(n)
+            cell.commit()
+            if n == 3:
+                self.assertTrue(cell.alive)
+            else:
+                self.assertFalse(cell.alive)
+
+        for n in range(9):
+            cell.alive = True
+            cell.update(n)
+            cell.commit()
+
+            if n in [2,3]:
+                self.assertTrue(cell.alive)
+            else:
+                self.assertFalse(cell.alive)
+                self.assertEqual(cell.age,0)
+
+    def testCellAddMethod(self):
+        a = Cell(0,0)
+        b = Cell(0,1)
+
+        self.assertEqual(a+b,0) # false false
+
+        a.alive = True
+
+        self.assertEqual(a+b,1) # true false
+
+        b.alive = True
+
+        self.assertEqual(a+b,2) # true true
+
+        a.alive = False
+
+        self.assertEqual(a+b,1) # false true
+
+    def testCellRaddMethod(self):
+        a = Cell(0,0)
+        
+        self.assertEqual(a+0,0)
+        self.assertEqual(a+0.0,0)
+
+        a.alive = True
+
+        self.assertEqual(a+0,1)
+        self.assertEqual(a+0.0,1)
+
+        a.alive = False
+
+        self.assertEqual(a+1,1)
+        self.assertEqual(a+1.0,1)
+
+        a.alive = True
+        
+        self.assertEqual(a+1,2)
+        self.assertEqual(a+1.0,2)
+
+        for n in range(0,8):
+            cells = [Cell(x,0) for x in range(n)]
+            self.assertEqual(sum(cells),0)
+            cells = [Cell(x,0,alive=True) for x in range(n)]
+            self.assertEqual(sum(cells),n)
+        
---
+++ tests/test_world.py
@@ -0,0 +1,201 @@
+
+import unittest
+
+from GameOfLife import Cell, Patterns
+from GameOfLife.world import World, OptimizedWorld
+
+
+class WorldTestCase(unittest.TestCase):
+
+    def assertIsWorld(self,obj,width=None,height=None,cellClass=None):
+
+        self.assertIsInstance(obj,World)
+
+        if width is not None:
+            self.assertEqual(obj.width,width)
+
+        if height is not None:
+            self.assertEqual(obj.height,height)
+
+        if cellClass is None:
+            cellClass = Cell
+            
+        self.assertEqual(obj.cellClass,cellClass)
+
+        self.assertEqual(len(obj.cells),obj.width * obj.height)
+        
+        for c in obj:
+            self.assertIsInstance(c,obj.cellClass)
+
+class WorldCreationTestCase(WorldTestCase):
+
+    def testWorldCreation(self):
+        
+        self.assertIsWorld(World())
+
+        class TestCell(Cell):
+            pass
+
+        self.assertIsWorld(World(CellClass=TestCell),cellClass=TestCell)
+
+        dim = 10
+        self.assertIsWorld(World(width=dim),width=dim)
+        self.assertIsWorld(World(height=dim),height=dim)
+        self.assertIsWorld(World(width=dim,height=dim),width=dim,height=dim)
+        
+        self.assertIsWorld(World(TestCell,dim,dim),
+                           width=dim,height=dim,cellClass=TestCell)
+
+
+class WorldPropertiesTestCase(WorldTestCase):
+
+    def testCellsProperty(self):
+        w = World()
+        self.assertIsInstance(w.cells,list)
+        self.assertEqual(len(w.cells),w.width * w.height)
+
+class WorldClassMethodsTestCase(WorldTestCase):
+
+    def testFromFile(self):
+        pass
+
+    def testFromString(self):
+        pass
+        
+class WorldInstanceMethodsTestCase(WorldTestCase):
+
+    def testWriteMethod(self):
+        pass
+
+    def testReadMethod(self):
+        pass
+
+    def testClampMethod(self):
+        w = World(width=10,height=10)
+        for y in range(10):
+            for x in range(10):
+                xy = (x,y)
+                self.assertSequenceEqual(xy,w._clamp(xy))
+
+        for y in range(10,20):
+            for x in range(10,20):
+                xy = (x,y)
+                XY = (x-10,y-10)
+                self.assertSequenceEqual(XY,w._clamp(xy))
+
+        # test wrapping here?
+
+    def testGetitemMethod(self):
+        world = World(width=10,height=10)
+        for cell in world:
+            self.assertTrue(cell is world[cell.location])
+
+    def testRestMethod(self):
+        
+        world = World(width=10,height=10)
+
+        self.assertEqual(len([c for c in world if c.alive]),0)
+
+        world[0,0].alive = True
+        world[1,0].alive = True
+        world[0,1].alive = True
+        world[1,1].alive = True
+
+        self.assertEqual(len([c for c in world if c.alive]),4)
+        
+        world.step()
+
+        self.assertEqual(len([c for c in world if c.alive]),4)
+        self.assertEqual(world.generation,1)
+
+        world.reset()
+        self.assertEqual(len(world.cells),world.width*world.height)
+        self.assertEqual(len([c for c in world if c.alive]),0)
+        self.assertEqual(world.generation,0)
+        
+
+    def testStepMethod(self):
+        world = World(width=10,height=10)
+        
+        # this is a static pattern for classic game of life rules
+        world[0,0].alive = True
+        world[0,1].alive = True
+        world[1,0].alive = True
+        world[1,1].alive = True
+        # these cells will never transition from alive to dead as
+        # long as no other neighbor cells become alive.
+
+        self.assertEqual(world[0,0].age,0)
+        self.assertEqual(world[0,1].age,0)
+        self.assertEqual(world[1,0].age,0)
+        self.assertEqual(world[1,1].age,0)
+
+        self.assertEqual(len([c for c in world if c.alive]),4)
+
+        for trip in range(10):
+            world.step()
+            alive_cells = [c for c in world if c.alive]
+            self.assertEqual(len(alive_cells),4)
+            self.assertEqual(world.generation,trip+1)
+            for cell in alive_cells:
+                self.assertEqual(cell.age,trip+1)
+                
+        world.reset()
+
+        world[0,0].alive = True
+
+        world.step()
+        self.assertEqual(len([c for c in world if c.alive]),0)
+        
+
+    def testAddMethod(self):
+
+        # needs world bounds checking for world smaller than pattern
+
+        for pattern in Patterns.values():
+            
+            world = World(width=40,height=40)
+            
+            nAlivePatternCells = len([c for c in pattern if not c.isspace()])
+
+            world.addPattern(pattern)
+
+            self.assertEqual(len([cell for cell in world if cell.alive]),
+                             nAlivePatternCells,
+                             'pattern {pattern}'.format(pattern=pattern))
+
+        for name,pattern in Patterns.items():
+            
+            world = World(width=40,height=40)
+            
+            nAlivePatternCells = len([c for c in pattern if not c.isspace()])
+
+            world.addPattern(name)
+
+            self.assertEqual(len([cell for cell in world if cell.alive]),
+                             nAlivePatternCells,
+                             'pattern {pattern}'.format(pattern=pattern))            
+            
+        # XXX test x,y, rule, eol and resize
+        
+
+
+class OptimizedWorldPropertiesTestCase(WorldTestCase):
+
+    def testAliveProperty(self):
+        pass
+
+class OptimizedWorldInstanceMethodsTestCase(WorldTestCase):
+
+    def testResetMethod(self):
+        pass
+
+    def testAddMethod(self):
+        pass
+
+    def testStepMethod(self):
+        pass
+        
+
+        
+        
