953c0ea1c245363b1a359ab5c094cba73544d310
Erik O'Shaughnessy <erik.oshaughnessy@gmail.com>
2015-11-18 15:33:06 -0600

    half-formed idea about a Pattern object

--- GameOfLife/patterns/__init__.py
+++ GameOfLife/patterns/__init__.py
@@ -6,12 +6,15 @@ Canned patterns.
 
 from pkg_resources import ResourceManager
 
+Patterns = {}
 _EXT = '.life'
 _rm = ResourceManager()
-Patterns = {}
 _pkg = 'GameOfLife.patterns.data'
-
 for fname in [f for f in _rm.resource_listdir(_pkg,'.') if f.endswith(_EXT)]:
     with _rm.resource_stream(_pkg,fname) as f:
         Patterns.setdefault(fname.split('.')[0].lower(),
                             f.read().decode('utf-8'))
+
+class Pattern(object):
+    def __init__(self,name,data=None):
+        pass
