f566d3b724b13c0ac2b5ff67c50883e8810a0840
Erik O'Shaughnessy <erik.oshaughnessy@gmail.com>
2015-11-18 15:49:54 -0600

    0.1.3

--- GameOfLife/__init__.py
+++ GameOfLife/__init__.py
@@ -9,7 +9,7 @@ world populated with cells.
 __author__ = '\n'.join(["Erik O'Shaughnessy",
                         'erik.oshaughnessy@gmail.com',
                         'https://github.com/JnyJny/GameOfLife'])
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 
 from .cell import Cell as Cell
 from .world import OptimizedWorld as World
